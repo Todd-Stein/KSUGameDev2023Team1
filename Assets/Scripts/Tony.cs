d449a0e5f871e4edff88ec5d85045515b67cb053
Justin L <jel2658@gmail.com>
2023-01-25 16:52:13 -0500

    Tony moves
    Managed to get Tony walking between an array of points on the navmesh.

--- Assets/Scripts/Tony.cs
+++ Assets/Scripts/Tony.cs
@@ -1,23 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Tony : MonoBehaviour
 {
     public bool hunting; // To be toggled when he is in hunt mode
     public bool alerted; // To be toggled when noise can/is heard by him
-    public float speed;
+    public float speed;  // Changes the speed that Tony moves
+    public Transform currentGoal; // The current goal for Tony to move to
+    public Transform[] goals; // An array of goals for Tony to move to
+    int goalIndex;
+
+    NavMeshAgent agent;
+    Transform personalTransform;
 
     // Start is called before the first frame update
     void Start()
     {
         hunting = false;
         alerted = false;
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = currentGoal.position;
+        personalTransform = GetComponent<Transform>();
+        goalIndex = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.speed = speed; // Change speed as necessary
+        if (checkGoals())
+        {
+            // Change currentGoal to next goal in goals[]
+            if (goalIndex < 3)
+            {
+                currentGoal = goals[++goalIndex].GetComponent<Transform>();
+            } else
+            {
+                goalIndex = 0;
+                currentGoal = goals[goalIndex].GetComponent<Transform>();
+            }
+            agent.destination = currentGoal.position;
+        }
+    }
+
+    bool checkGoals()
+    {
+        if (currentGoal.position.x == personalTransform.position.x &&
+            currentGoal.position.z == personalTransform.position.z)
+        {
+            return true;
+        }
+        return false;
     }
 }
