f140531f1596d90bebb9c676ef925b98fe59bc1a
Justin L <jel2658@gmail.com>
2023-01-25 16:07:48 -0500

    Tony & NavMesh
    Added Tony's hunting bool, alerted bool, and speed float. Created a navmesh for Tony Scene.

--- Assets/Scripts/Tony.cs
+++ Assets/Scripts/Tony.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Tony : MonoBehaviour
 {
+    public bool hunting; // To be toggled when he is in hunt mode
+    public bool alerted; // To be toggled when noise can/is heard by him
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hunting = false;
+        alerted = false;
     }
 
     // Update is called once per frame
