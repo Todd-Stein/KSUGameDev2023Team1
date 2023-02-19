6068cffa7b419bfcbda97b8e0d2f58b393eded5c
Jkirby243 <60945819+Jkirby243@users.noreply.github.com>
2023-02-19 14:27:57 -0500

    Added sound
    Fixed the environment triggers and added scripts to all interactables and a test sound

--- Assets/Scripts/System_Scripts/SoundEvent.cs
+++ Assets/Scripts/System_Scripts/SoundEvent.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 //Sound System for making sound events
 //Sound objects will need a sphere collider attatched to act as a trigger; Needs to be disabled inistially
@@ -10,6 +11,9 @@ public class SoundEvent : MonoBehaviour
     public float noiseradius;
     private SphereCollider soundtrigger;
     float timer = 0;
+
+    public AudioClip sound;
+    private AudioSource au;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +26,8 @@ public class SoundEvent : MonoBehaviour
         
         soundtrigger.enabled = false;
         soundtrigger.radius = noiseradius;
+        au = gameObject.GetComponent<AudioSource>();
+        au.clip = sound;
     }
 
     // Update is called once per frame
@@ -48,5 +54,6 @@ public class SoundEvent : MonoBehaviour
     public void activateNoiseEvent()
     {
         soundtrigger.enabled = true;
+        au.Play();
     }
 }
