3e214d8df223c6eb1c6a3309bbfe28a6246bffb5
Jkirby243 <60945819+Jkirby243@users.noreply.github.com>
2023-03-06 12:53:32 -0500

    Added in the additions of the sound manipulation
    Added in the sound manipulation stuff and will debug later

--- Assets/Scripts/System_Scripts/SoundListener.cs
+++ Assets/Scripts/System_Scripts/SoundListener.cs
@@ -21,11 +21,11 @@ public class SoundListener : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<SoundEvent>() && !other.GetComponent<Door>() && other.GetComponent<SphereCollider>().enabled == true)
+        if (other.gameObject.layer == 6 && other.isTrigger || other.gameObject.layer == 7 && other.isTrigger)
         {
             soundlocation = other.gameObject;
-            tony.GetComponent<Tony>().OnAlert(soundlocation, soundlocation.GetComponent<SoundEvent>().noise); // Call Tony's alert script, increasing aggression by noise level
-            Debug.Log("Heared sound event");
+            tony.GetComponent<Tony>().OnAlert(other.gameObject, other.GetComponent<SoundEvent>().noise); // Call Tony's alert script, increasing aggression by noise level
+            Debug.Log("Heared sound event" + other.name + " other.enebled = " + other.enabled);
         }
     }
 }
