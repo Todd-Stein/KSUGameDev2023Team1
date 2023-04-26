03eb57ec48f7c56d3a4b27c1d7922cf9bcfa3e1a
Inferius77 <inferius77@live.com>
2023-04-26 00:09:02 -0400

    more additions
    Click, back, hover sounds added.
    creditscanvas added.

--- Assets/Scripts/MainMenu/MainMenuButtons.cs
+++ Assets/Scripts/MainMenu/MainMenuButtons.cs
@@ -5,6 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuButtons : MonoBehaviour
 {
+
+    [SerializeField]
+    private AudioClip[] UIsounds;
+
+    private AudioSource audioSource;
+    private GameObject creditsCanvas;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        creditsCanvas = GameObject.Find("CreditsCanvas");
+        creditsCanvas.SetActive(false);
+    }
+
     public void LoadMainMap()
     {
         Debug.Log("Start pressed");
@@ -20,12 +34,33 @@ public class MainMenuButtons : MonoBehaviour
     public void ShowCreditsOverlay()
     {
         Debug.Log("Credits pressed");
+        creditsCanvas.SetActive(true);
+    }
 
+    public void HideCreditsOverlay()
+    {
+        Debug.Log("Exit Credits");
+        creditsCanvas.SetActive(false);
     }
 
     public void ExitGame()
     {
         Debug.Log("Exit pressed");
         Application.Quit();
+    }
+
+    public void ClickNoise()
+    {
+        audioSource.PlayOneShot(UIsounds[0]);
+    }
+
+    public void BackNoise()
+    {
+        audioSource.PlayOneShot(UIsounds[1]);
+    }
+
+    public void HoverNoise()
+    {
+        audioSource.PlayOneShot(UIsounds[2]);
     }
 }
