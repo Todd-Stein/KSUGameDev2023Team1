9e241f28a400801a951e4bbef302dc778a89813f
Justin L <jel2658@gmail.com>
2023-05-04 16:39:38 -0400

    Update TonyAnimator, player_health, Checkpoint, Tony
    Not sure why TonyAnimator was updated, I think I was checking whether it called correctly.
    Not sure why Main Map was updated.
    player_health: deleted Start() since it is not used.
    Checkpoint: Disabled Tony hunt mode ability on Death. Re-enabled when he is reloaded.
    Tony: Added canHunt bool for whether he can hunt or not. Edited agent calls to check if agent is enabled first. Edited swipe calls to make sure the Tony is hunting, that he can hunt, that the agent is enabled, and that the player is within 5  units of Tony. No longer calls rage trigger twice when going into hunt mode.

--- Assets/Scripts/player_health.cs
+++ Assets/Scripts/player_health.cs
@@ -36,13 +36,6 @@ public class player_health : MonoBehaviour
         bloodOverlay.SetActive(false);
     }
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
