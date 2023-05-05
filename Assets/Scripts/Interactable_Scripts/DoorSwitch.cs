e1bf726f2b45a9e8847a9479cb68ac5af8754909
Jkirby243 <60945819+Jkirby243@users.noreply.github.com>
2023-05-04 20:35:10 -0400

    Sounds and and all sound necessary events

--- Assets/Scripts/Interactable_Scripts/DoorSwitch.cs
+++ Assets/Scripts/Interactable_Scripts/DoorSwitch.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class DoorSwitch : MonoBehaviour
 {
     public Door Door_;
-
+    private AudioSource au;
+    private void Start()
+    {
+        au = GetComponent<AudioSource>();
+    }
     public void Unlock()
     {
+        au.Play();
         Door_.unlocked = true;
         Debug.Log(Door_.name + " unlocked values is equal to " + Door_.unlocked);
     }
