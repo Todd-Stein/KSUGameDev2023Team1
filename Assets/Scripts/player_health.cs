9b3461772b062896f979200d0275373583133ed5
Jkirby243 <60945819+Jkirby243@users.noreply.github.com>
2023-05-08 12:19:13 -0400

    New player damage visual

--- Assets/Scripts/player_health.cs
+++ Assets/Scripts/player_health.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class player_health : MonoBehaviour
 {
@@ -25,6 +26,12 @@ public class player_health : MonoBehaviour
     public AudioClip playerHurt;
     public float hurtAudioVolume = 1.0f;
 
+    public bool bloodact = false;
+
+    private Color tmpcolor;
+    private float bloodtimer = 2f;
+    private float btime = 100000000;
+
     private void Awake()
     {
         currentHealth = totalHealth;
@@ -32,18 +39,42 @@ public class player_health : MonoBehaviour
         {
             bloodOverlay = transform.GetChild(0).GetChild(0).GetChild(2).gameObject;
         }
-        bloodMaterialRef = bloodOverlay.GetComponent<MeshRenderer>();
+        //bloodMaterialRef = bloodOverlay.GetComponent<MeshRenderer>();
         bloodOverlay.SetActive(false);
+
+        tmpcolor = bloodOverlay.GetComponent<Image>().color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
         /*if (Input.GetKeyDown(KeyCode.Space))
         {
             RecieveHit();
         }*/
+
+        if (bloodact)
+        {
+            if(tmpcolor.a <= .7 - currentHealth * .1)
+            {
+                tmpcolor.a += 2 * Time.deltaTime;
+            }
+            else
+            {
+                bloodact = false;
+            }
+            bloodOverlay.GetComponent<Image>().color = tmpcolor;
+        }
+
+        if(Time.time >= btime + bloodtimer)
+        {
+            if (tmpcolor.a >= 0)
+            {
+                tmpcolor.a -= 2 * Time.deltaTime;
+            }
+            bloodOverlay.GetComponent<Image>().color = tmpcolor;
+        }
     }
     public void RecieveHit()
     {
@@ -54,6 +85,8 @@ public class player_health : MonoBehaviour
             AudioSource.PlayClipAtPoint(playerHurt, transform.position, hurtAudioVolume);
 
         bloodOverlay.SetActive(true);
+        bloodact = true;
+        btime = Time.time;
         /*if (bloodMaterialRef.material != null)
         {
             bloodMaterialRef.material.SetFloat("_randomizeBlood1", Random.Range(200, 600));
